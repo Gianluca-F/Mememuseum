ba1b0730543d76e1b6d92fd67c0a9909526761f7
Gianluca Fiorentino <122386633+Gianluca-F@users.noreply.github.com>
2025-07-06 17:06:17 +0200

    Created middleware for auth.

---
+++ server/middlewares/authorizarion.js
@@ -0,0 +1,20 @@
+import { isTokenValid } from '../utils/JwtUtils';
+
+const SECRET_KEY = process.env.TOKEN_SECRET || 'una_chiave_segreta_di_sviluppo';
+
+export function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader?.split(' ')[1];
+
+  if (!token) {
+    throw { status: 401, message: 'Unauthorized' };
+  }
+
+  isTokenValid(token, (err, user) => {
+    if (err) {
+      throw { status: 401, message: 'Unauthorized' };
+    }
+    req.user = user;
+    next();
+  });
+}
