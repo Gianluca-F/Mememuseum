883c1f52ed548d5e6171dac6a1fdc5effddfbf48
Gianluca Fiorentino <122386633+Gianluca-F@users.noreply.github.com>
2025-07-09 12:48:45 +0200

    Verify sanitizeHtml

--- server/index.js
+++ server/index.js
@@ -2,9 +2,6 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import helmet from 'helmet';
-import xss from 'xss-clean';
-import csurf from 'csurf';
-import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 
 import { authenticationRouter } from './routes/authenticationRouter.js';
@@ -13,6 +10,7 @@ import { commentRouter } from './routes/commentRouter.js';
 import { apiDocsRouter } from './utils/swaggerUI.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { authenticateToken } from './middlewares/authorization.js';
+import { sanitizeInput } from './middlewares/sanitize.js';
 
 // Configurazione dotenv per variabili ambiente
 dotenv.config();
@@ -25,12 +23,9 @@ app.use(morgan('dev'));
 
 // Middleware di sicurezza
 app.use(helmet());
-//app.use(xss()); // NOTE: commentato per Swagger UI, da abilitare in produzione
 app.use(cors());
-//app.use(csurf({ cookie: true })); // NOTE: commentato per Swagger UI, da abilitare in produzione
 
-// Middleware per il parsing
-app.use(cookieParser());
+// Middleware per il parsing del corpo delle richieste JSON
 app.use(express.json());
 
 // Serve i file statici dalla cartella uploads
@@ -39,11 +34,13 @@ app.use('/uploads', express.static('uploads'));
 // Setup Swagger UI per la documentazione API in /api-docs
 app.use(apiDocsRouter);
 
+// Middleware per la sanitizzazione degli input
+app.use(sanitizeInput); // NOTE: Verificare che non causi problemi
+
 // Definizione delle routes
 app.use('/auth', authenticationRouter);
 app.use('/memes', memeRouter);
-app.use(authenticateToken);
-app.use('/memes/:memeId/comments', commentRouter); // Autenticazione richiesta
+app.use('/memes/:memeId/comments', authenticateToken, commentRouter);
 
 // Middleware per gestire gli errori
 app.use(errorHandler);
