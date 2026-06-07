0174925a92312498a79c4ef3bee98ea25f24f96b
Gianluca Fiorentino <122386633+Gianluca-F@users.noreply.github.com>
2026-06-07 18:04:57 +0200

    removed useless stuff

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideToastr } from 'ngx-toastr';
-import { provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './_services/api/auth.interceptor';
